0ed4655f51daa25457046372a0e2b51d5b94c8d5
maiqel <michaelolowe321@gmail.com>
2024-09-29 00:50:21 +0100

    feat:addf

---
+++ components/ui/toolbar.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface ToolbarProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Toolbar = React.forwardRef<HTMLDivElement, ToolbarProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          "flex flex-wrap items-center gap-1 rounded-md border p-1",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+Toolbar.displayName = "Toolbar";
+
+export { Toolbar };
