46350c1507bb676d489632ccbac7a8fbe1fc8fc8
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-08 20:15:15 +0100

    feat: implemented connect-wallet page

---
+++ app/(authentication)/auth/connect-wallet/page.tsx
@@ -0,0 +1,11 @@
+import ConnectWallet from "@/app/components/auth/connect-wallet";
+
+const ConnectWalletPage = () => {
+  return (
+    <main className="pt-24 min-h-dvh">
+      <ConnectWallet />
+    </main>
+  );
+};
+
+export default ConnectWalletPage;
