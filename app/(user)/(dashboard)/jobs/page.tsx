c1a0e4251b9064dd5565065d0207afe0586e2f5c
Peter Abah <peterabah.ob@gmail.com>
2024-10-12 11:47:28 +0100

    feat(job-listing): add header for jobs page

--- app/(user)/(dashboard)/jobs/page.tsx
+++ app/(user)/(dashboard)/jobs/page.tsx
@@ -1,14 +1,17 @@
 import { JobCard } from "@/components/job";
+import { JobsHeader } from "@/components/job/jobs-header";
 import { JobDummyData } from "@/data/mock/job";
 
 const Jobs = () => {
   return (
-    <div className="containerHeight overflow-scroll custom-scroll pb-6">
-      {JobDummyData.map((item, index) => (
-        <JobCard key={index} job={item} />
-      ))}
+    <div className="containerHeight overflow-scroll custom-scroll pb-6 space-y-6">
+      <JobsHeader />
+      <div>
+        {JobDummyData.map((item, index) => (
+          <JobCard key={index} job={item} />
+        ))}
+      </div>
     </div>
   );
 };
-
 export default Jobs;
