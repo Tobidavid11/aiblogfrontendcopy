41f82cee35f569df8d1e5bb756970e0bf08cf25e
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-22 23:28:47 +0100

    fix: reverted

--- schemas/blog.ts
+++ schemas/blog.ts
@@ -2,14 +2,6 @@ import { z } from "zod";
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024;
 
-// const isServer = typeof window === "undefined";
-
-const fileSchema = z.object({
-	name: z.string(),
-	size: z.number(),
-	type: z.string(),
-});
-
 export const createBlogSchema = z.object({
 	title: z
 		.string()
@@ -20,24 +12,11 @@ export const createBlogSchema = z.object({
 	category: z.string().min(1, "Category is required"),
 	status: z.string().default("published"),
 	coverImage: z
-		.union([
-			z
-				.instanceof(File)
-				.refine((file) => file.size <= MAX_FILE_SIZE, "Max file size is 5MB.")
-				.refine(
-					(file) =>
-						["image/jpeg", "image/png", "image/webp"].includes(file.type),
-					"Only .jpg, .png, and .webp formats are supported.",
-				),
-			z.string().url("Invalid image URL"),
-			fileSchema
-				.refine((file) => file.size <= MAX_FILE_SIZE, "Max file size is 5MB.")
-				.refine(
-					(file) =>
-						["image/jpeg", "image/png", "image/webp"].includes(file.type),
-					"Only .jpg, .png, and .webp formats are supported.",
-				),
-		])
-		.optional()
-		.nullable(),
+		.instanceof(File)
+		.refine((file) => file.size <= MAX_FILE_SIZE, "Max file size is 5MB.")
+		.refine(
+			(file) => ["image/jpeg", "image/png", "image/webp"].includes(file.type),
+			"Only .jpg, .png, and .webp formats are supported.",
+		)
+		.optional(),
 });
